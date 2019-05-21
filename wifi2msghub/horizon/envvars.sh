e6ebf6edb381d794e3b556534cc834c317175c10
Troy Fine <troy.fine@yahoo.com>
2019-05-21 15:30:39 -0700

    Update envvars.sh

--- wifi2msghub/horizon/envvars.sh
+++ wifi2msghub/horizon/envvars.sh
@@ -1,5 +1,6 @@
-
-export HZN_DEVICE_TOKEN='wifi_scan_sample'
+export HZN_ORG_ID='<HZN_ORG_ID>'
+export HZN_DEVICE_ID='<HZN_DEVICE_ID>'
+export HZN_DEVICE_TOKEN='<HZN_DEVICE_TOKEN>'
 export HZN_EXCHANGE_USER_AUTH='iamapikey:<YOUR API KEY>'
 export MYDOMAIN=ibm.sample.wifi2msghub
 export HZN_PATTERN=wifi2msghub
